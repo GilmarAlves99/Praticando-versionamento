54dead404bcc24ba960bad9b0fe10da465b3f2c7
Gilmar Alves <GilmarAlves914@gmail.com>
2023-03-12 22:54:43 -0300

    Corrigindo cores e tamanhos de fotos

--- animationScroll.js
+++ animationScroll.js
@@ -2,9 +2,10 @@ const boxes = document.querySelectorAll(".box")
 
 window.addEventListener('scroll', checkBoxes)
 checkBoxes();
+
 function checkBoxes() {
-    const triggerBottom = (window.innerHeight / 4* 3);
-  
+    const triggerBottom = (window.innerHeight / 4 * 3.3);
+
     boxes.forEach((box) => {
         const boxTop = box.getBoundingClientRect().top;
 
@@ -16,4 +17,3 @@ function checkBoxes() {
     });
 
 }
-
