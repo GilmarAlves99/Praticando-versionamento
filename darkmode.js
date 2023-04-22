f5ed4b1be72ed209706e5febc398c6f58f79d6ec
Gilmar Alves <gilmaralves914@gmail.com>
2023-04-22 15:49:49 -0300

    Corrigindo cores

--- darkmode.js
+++ darkmode.js
@@ -2,10 +2,16 @@ const darkButton = document.getElementById("darkInput");
 
 const branco = document.documentElement;
 const preto = document.documentElement;
+const verdeClaro = document.documentElement;
+const verdeEscuro = document.documentElement;
 
 function padraocores() {
     branco.style.setProperty('--branco', '#fff')
     preto.style.setProperty('--preto', "#000");
+    verdeClaro.style.setProperty('--verdeClaro', "#8AA6A3");
+    verdeEscuro.style.setProperty('--verdeEscuro', "#10403B");
+
+
 }
 
 
@@ -19,6 +25,8 @@ function ativadoounao() {
         console.log("ativado")
         branco.style.setProperty('--branco', '#000')
         preto.style.setProperty('--preto', '#fff')
+        verdeClaro.style.setProperty('--verdeClaro', '#10403B')
+        verdeEscuro.style.setProperty('--verdeEscuro', '#8AA6A3')
 
     } else if (darkButton.checked == false) {
         console.log("desativado")
